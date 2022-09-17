2362c3020d0ff67436ca7b0b7346dd71116a5602
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2022-09-17 13:04:21 +0300

    added blur and call window

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+console.log(btnsShowModalWindow);
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].addEventListener('click', function () {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
