cd30873422839205df5eae429d42b18403632951
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2022-09-17 13:44:07 +0300

    added closing function and code refactoring

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const btnCloseModalWindow = document.querySelector('.close-modal-window');
 const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
 console.log(btnsShowModalWindow);
 
+const showModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  //  modalWindow.style.display = 'block';
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsShowModalWindow.length; i++) {
-  btnsShowModalWindow[i].addEventListener('click', function () {
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsShowModalWindow[i].addEventListener('click', showModalWindow);
 }
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
